4ded15310a4c6fa98d56d48fbcca7817e092f00f
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-13 14:10:30 +0700

    Add list.

--- kotlincollection/src/main/kotlin/list/List.kt
+++ kotlincollection/src/main/kotlin/list/List.kt
@@ -1,5 +1,33 @@
 package list
 
 fun main() {
-    println("Test list")
+    immutableList()
+    mutableList()
+}
+
+fun immutableList() {
+    val list: List<String> = listOf("Taufik", "Hidayat")
+    println(list[0])
+    println(list[1])
+    println(list.isEmpty()) // same as println(list.size == 0)
+    println(list.indexOf("Taufik"))
+    println(list.indexOf("Hidayat"))
+}
+
+fun mutableList() {
+    val mutableList: MutableList<String> = mutableListOf()
+    mutableList.add("Taufik")
+    mutableList.add("Hidayat")
+    mutableList.add("Apel")
+    mutableList.add("Mangga")
+    mutableList.add("Pisang")
+    println(mutableList[0])
+    println(mutableList[1])
+    println(mutableList[2])
+    println(mutableList.isEmpty())
+
+    println("List sebelum diubah: $mutableList")
+    mutableList[2] = "Anggur"
+    mutableList.removeAt(1)
+    println("List setelah diubah: $mutableList")
 }
