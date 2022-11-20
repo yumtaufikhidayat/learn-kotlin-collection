6b08b30028b1f8bafe34e03d1538283a08ac4ffb
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-20 09:06:52 +0700

    Add set.

--- kotlincollection/src/main/kotlin/set/Set.kt
+++ kotlincollection/src/main/kotlin/set/Set.kt
@@ -23,6 +23,10 @@ fun mutableSet() {
     mutableSet.add(Person("Zharfan Wafiq"))
     mutableSet.add(Person("Eko Cahyadi"))
 
-    println("Jumlah data pada mutable set: ${mutableSet.size}")
+    println("Jumlah data awal pada mutable set: ${mutableSet.size}")
+    println("Eko Cahyadi berada pada index ke-${mutableSet.indexOf(Person("Eko Cahyadi"))}")
+    println("Eko Cahyadi dihapus dari mutable set: ${mutableSet.remove(Person("Eko Cahyadi"))}")
+    println("Eko Cahyadi berada pada index ke-${mutableSet.indexOf(Person("Eko Cahyadi"))}")
+    println("Jumlah data terkini pada mutable set: ${mutableSet.size}")
     println(mutableSet.contains(Person("Eko Cahyadi")))
 }
