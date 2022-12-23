5e854fe04e05931f2c70ce501cbd45f8935eaa25
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2022-12-23 15:37:50 +0700

    Implement collection operations.

--- kotlincollection/src/main/kotlin/collection/CollectionOperations.kt
+++ kotlincollection/src/main/kotlin/collection/CollectionOperations.kt
@@ -5,8 +5,23 @@ fun main() {
 }
 
 fun collectionOperations() {
+//  Modern approach
     listOf("Taufik", "Hidayat").forEach { value -> println(value) }
+
+//  Traditional approach
+    val data = listOf("Taufik", "Hidayat")
+    for (i in data) {
+        println(i)
+    }
+
+//  Modern approach
     listOf("Taufik", "Hidayat", "Zharfan", "Wafiq").forEachIndexed { index, value ->
+        println("$index = $value")
+    }
+
+//  Traditional approach
+    val dataIndex = listOf("Taufik", "Hidayat", "Zharfan", "Wafiq")
+    for ((index, value) in dataIndex.withIndex()) {
         println("$index = $value")
     }
 }
