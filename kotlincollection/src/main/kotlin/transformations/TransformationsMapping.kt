940e0ef00975273f8dbcadfad76c9ef83460a2d2
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2022-12-28 15:59:26 +0700

    Implement transformations mapping for map.

--- kotlincollection/src/main/kotlin/transformations/TransformationsMapping.kt
+++ kotlincollection/src/main/kotlin/transformations/TransformationsMapping.kt
@@ -2,9 +2,11 @@ package main.kotlin.transformations
 
 fun main() {
     transformationsMapping()
+    transformationsMappingMap()
 }
 
 fun transformationsMapping() {
+    println("----- Transformations Mapping-----")
     val list: List<String> = listOf("Taufik", "Hidayat", "Zharfan", "Wafiq")
     val transformsList1: List<String> = list.map { item ->
         item.toUpperCase()
@@ -21,4 +23,19 @@ fun transformationsMapping() {
     println(transformsList1)
     println(transformsList2)
     println(transformsList3)
+}
+
+fun transformationsMappingMap() {
+    println("\n----- Transformations Mapping for Map-----")
+    val map: Map<Int, String> = mapOf(
+        1 to "Taufik",
+        2 to "Hidayat",
+        3 to "Zharfan",
+        4 to "Wafiq"
+    )
+    val transformsMap1 = map.mapKeys { it.key * 10 }
+    val transformsMap2 = map.mapValues { it.value.toUpperCase() }
+
+    println(transformsMap1)
+    println(transformsMap2)
 }
