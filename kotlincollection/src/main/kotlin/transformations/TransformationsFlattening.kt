b1a2de9d5b602233a7891ccf0cfb8590d785d1e8
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-01-08 06:03:01 +0700

    Implement transformations flattening.

---
+++ kotlincollection/src/main/kotlin/transformations/TransformationsFlattening.kt
@@ -0,0 +1,27 @@
+package transformations
+
+fun main() {
+    transformationsFlattening()
+    memberHobbies()
+}
+
+fun transformationsFlattening() {
+    val list: List<List<String>> = listOf(
+        listOf("Taufik", "Hidayat"),
+        listOf("Android", "Developer")
+    )
+    val flattenList: List<String> = list.flatten()
+    println(flattenList)
+}
+
+class Member(val name: String, val hobbies: List<String>)
+
+fun memberHobbies() {
+    val members: List<Member> = listOf(
+        Member("Taufik Hidayat", listOf("Travelling", "Gaming")),
+        Member("Zharfan Wafiq", listOf("Working", "Studying"))
+    )
+
+    val hobbies: List<String> = members.flatMap { member -> member.hobbies }
+    println("hobbies: $hobbies")
+}
