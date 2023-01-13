779aeeb2ae199c5c4b245f719a02658032748690
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-01-13 19:22:19 +0700

    Implement transformations filtering.

--- kotlincollection/src/main/kotlin/filter/Filtering.kt
+++ kotlincollection/src/main/kotlin/filter/Filtering.kt
@@ -7,10 +7,11 @@ fun main() {
 fun filtering() {
     val list1 = listOf("Taufik", "Hidayat", "Zharfan", "Wafiq")
     val list2 = list1.filter { it.length > 5 }
-    val list3 = list1.filterIndexed { index, value -> index % 2 == 0 }
+    val list3 = list1.filterIndexed { index, _ -> index % 2 == 0 }
     val list4: List<Any?> = listOf(null, 1, "Taufik", "Hidayat", 2, null)
     val list5: List<String> = list4.filterIsInstance<String>()
     val list6: List<Any> = list4.filterNotNull()
+    val list7 = list1.filterNot { it.length > 5 }
 
     println("---$list1---")
     println("---$list2---")
@@ -18,4 +19,5 @@ fun filtering() {
     println("---$list4---")
     println("---$list5---")
     println("---$list6---")
+    println("---$list7---")
 }
