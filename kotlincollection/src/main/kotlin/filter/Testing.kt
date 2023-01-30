4ca8cddef65bdd165aa2b6bb0f7c3f37678de76b
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-01-30 19:56:48 +0700

    Implement testing.

--- kotlincollection/src/main/kotlin/filter/Testing.kt
+++ kotlincollection/src/main/kotlin/filter/Testing.kt
@@ -6,9 +6,9 @@ fun main() {
 
 fun testing() {
     val list = listOf("Taufik", "Hidayat", "Zharfan", "Wafiq")
-    println(list.any { it. length > 5})
-    println(list.none { it. length > 5})
-    println(list.all { it. length > 5})
+    println(list.any { it.length > 5})
+    println(list.none { it.length > 5})
+    println(list.all { it.length > 5})
     println(list.any())
     println(list.none())
 }
