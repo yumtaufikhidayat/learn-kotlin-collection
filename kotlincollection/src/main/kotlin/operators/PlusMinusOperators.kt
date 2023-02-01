0c94e589c0f301f01900c695c2a61e35abbd8fa6
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-02-01 19:20:12 +0700

    Implement plus minus operators.

--- kotlincollection/src/main/kotlin/operators/PlusMinusOperators.kt
+++ kotlincollection/src/main/kotlin/operators/PlusMinusOperators.kt
@@ -6,10 +6,10 @@ fun main() {
 
 fun plusMinusOperators() {
     val list1 = listOf("Taufik", "Hidayat", "Zharfan", "Wafiq")
-    val list2 = list1 + "Developer"
-    val list3 = list1 + listOf("Kotlin", "Java", "Python")
-    val list4 = list1 - "Taufik"
-    val list5 = list1 - listOf("Hidayat", "Zharfan", "Wafiq")
+    val list2 = list1.plus("Developer")
+    val list3 = list1.plus(listOf("Kotlin", "Java", "Python"))
+    val list4 = list1.minus("Taufik")
+    val list5 = list1.minus(listOf("Hidayat", "Zharfan", "Wafiq"))
     println("list1: $list1")
     println("list2: $list2")
     println("list3: $list3")
@@ -17,8 +17,8 @@ fun plusMinusOperators() {
     println("list5: $list5")
 
     val map1 = mapOf("a" to "Taufik", "b" to "Hidayat")
-    val map2 = map1 + ("c" to "Zharfan")
-    val map3 = map1 - "a"
+    val map2 = map1.plus("c" to "Zharfan")
+    val map3 = map1.minus("a")
     println("map1: $map1")
     println("map2: $map2")
     println("map3: $map3")
