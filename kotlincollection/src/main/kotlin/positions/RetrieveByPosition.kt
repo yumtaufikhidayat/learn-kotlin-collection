fc1f4f8eb0805da170c841500815ff39125d3751
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-02-16 19:23:22 +0700

    Implement retrieve by position.

--- kotlincollection/src/main/kotlin/positions/RetrieveByPosition.kt
+++ kotlincollection/src/main/kotlin/positions/RetrieveByPosition.kt
@@ -6,9 +6,11 @@ fun main() {
 
 fun retrieveByPosition() {
     val list = listOf("Taufik", "Hidayat", "Zharfan", "Wafiq")
-    println(list.first())
-    println(list.last())
-    println(list.elementAt(1))
-    println(list.elementAtOrNull(5))
-    println(list.elementAtOrElse(5) { "" })
+    println(list.first()) // Taufik
+    println(list[0]) // Taufik
+    println(list.elementAt(0)) // Taufik
+    println(list.last()) // Wafiq
+    println(list.elementAt(1)) // Zharfan
+    println(list.elementAtOrNull(5)) // null
+    println(list.elementAtOrElse(5) { "No data available" }) // No data available
 }
