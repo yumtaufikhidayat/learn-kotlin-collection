848bc74582614e462ac4211624f6c1df537333f9
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-02-16 20:01:17 +0700

    Implement retrieve by condition.

--- kotlincollection/src/main/kotlin/positions/RetrieveByCondition.kt
+++ kotlincollection/src/main/kotlin/positions/RetrieveByCondition.kt
@@ -6,8 +6,8 @@ fun main() {
 
 fun retrieveByCondition() {
     val list = (1..20).toList()
-    println(list.first { position -> position > 10 }) // 11
-    println(list.last { position -> position > 10 }) // 20
+    println(list.first { it > 10 }) // 11
+    println(list.last { it > 10 }) // 20
     println(list.firstOrNull { it > 20 }) // null
     println(list.lastOrNull { it > 20 }) //null
     println(list.find { it > 20 }) // null
