ea817c50e6dd92f8ff17ba8c675f10dcdcbabe48
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-02-18 06:38:07 +0700

    Implement retrieve by random.

---
+++ kotlincollection/src/main/kotlin/positions/RetrieveByRandom.kt
@@ -0,0 +1,13 @@
+package main.kotlin.positions
+
+fun main() {
+    retrieveByRandom()
+}
+
+fun retrieveByRandom() {
+    val list = (1..10).toList()
+    println(list.random())
+    println(list.random())
+    println(list.random())
+    println(list.random())
+}
