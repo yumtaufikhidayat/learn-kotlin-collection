a5cceaa4732ce5106a564091e7be968940712802
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-03-10 19:23:30 +0700

    Implement random order.

---
+++ kotlincollection/src/main/kotlin/order/RandomOrder.kt
@@ -0,0 +1,14 @@
+package main.kotlin.order
+
+fun main() {
+    randomOrder()
+}
+
+fun randomOrder() {
+    val list = (1..10).toList()
+    println(list.shuffled())
+    println(list.shuffled())
+    println(list.shuffled())
+    println(list.shuffled())
+    println(list.shuffled())
+}
