d613e9ab3bd19357037bde0bd0b2f8738420caed
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-03-16 16:47:40 +0700

    Implement aggregate operator.

---
+++ kotlincollection/src/main/kotlin/operators/AggregateOperator.kt
@@ -0,0 +1,13 @@
+package main.kotlin.operators
+
+fun main() {
+    aggregateOperator()
+}
+
+fun aggregateOperator() {
+    val numbers = (1..100).toList()
+    println("Maximum number: ${numbers.max()}") // take maximum number
+    println("Minimum number: ${numbers.min()}") // take minimum number
+    println("Average number: ${numbers.average()}") // take average number
+    println("Sum numbers: ${numbers.sum()}") // take total of sum number
+}
