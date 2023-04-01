eda86fe7b40eca1ebece7a6212d2e8b8f29ce215
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-04-01 13:47:20 +0700

    Implement set specific operator.

--- kotlincollection/src/main/kotlin/operators/SetSpecificOperator.kt
+++ kotlincollection/src/main/kotlin/operators/SetSpecificOperator.kt
@@ -9,8 +9,10 @@ fun setSpecificOperator() {
     val number2 = (6..15).toSet()
     val union = number1 union number2
     val intersect = number1 intersect number2
-    val subtract = number1 subtract number2
+    val subtractA = number1 subtract number2
+    val substactB = number2 subtract number1
     println("Gabungan: $union") // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     println("Irisan: $intersect") // [6, 7, 8, 9, 10]
-    println("Pengurangan: $subtract") // [1, 2, 3, 4, 5]
+    println("Selisih A: $subtractA") // [1, 2, 3, 4, 5]
+    println("Selisih B: $substactB") // [11, 12, 13, 14, 15]
 }
