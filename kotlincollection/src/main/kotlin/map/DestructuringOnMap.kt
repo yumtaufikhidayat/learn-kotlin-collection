282fd6aff87c50ebdf9bbf33dda593b524fa8f85
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-04-07 09:09:48 +0700

    Implement destructuring on map.

--- kotlincollection/src/main/kotlin/map/DestructuringOnMap.kt
+++ kotlincollection/src/main/kotlin/map/DestructuringOnMap.kt
@@ -5,5 +5,19 @@ fun main() {
 }
 
 fun destructuringOnMap() {
+    val map = mapOf(
+        "a" to "Taufik",
+        "b" to "Hidayat",
+        "c" to "Zharfan",
+        "d" to "Wafiq"
+    )
 
+    for ((key, value) in map) {
+        println("$key, $value")
+    }
+
+    map.forEach { (key, value) -> println("$key, $value") }
+    map.forEach { entry ->
+        println("${entry.component1()}: ${entry.component2()}")
+    }
 }
