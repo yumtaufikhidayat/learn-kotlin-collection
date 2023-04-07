f6af73f14068d0c9bf178bbcc5835e5fe53756b3
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-04-07 08:01:01 +0700

    Implement learn properties on map.

---
+++ kotlincollection/src/main/kotlin/map/PropertiesOnMap.kt
@@ -0,0 +1,19 @@
+package main.kotlin.map
+
+fun main() {
+    propertiesOnMap()
+}
+
+fun propertiesOnMap() {
+    val application = Application(mapOf(
+        "name" to "Kotlin App",
+        "version" to 1
+    ))
+    println("Application name: ${application.name}")
+    println("Application version: ${application.version}")
+}
+
+class Application(map: Map<String, Any>) {
+    val name: String by map
+    val version: Int by map
+}
